4369f8645d2e2a9dcc2372d08a03f39ad24572c1
Vikky Sigh <vikkyhp0007@gmail.com>
2021-06-30 16:50:51 +0530

    React Hassel eCom

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+//import the React and ReactDom lib
+import React from "react";
+
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//take the react component and show it on the screen
